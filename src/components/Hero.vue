b33ae40c4ca2a2ac64bf3009b8cc2d1af9cec24a
sigmanificient <sigmanificient@gmail.com>
2021-12-16 10:44:43 +0100

    :recycle: Extracting GhButton logic

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,5 +1,6 @@
 <script setup>
-  import BlueButton from '../components/BlueButton.vue'
+  import BlueButton from './buttons/BlueButton.vue'
+  import GhButton from './buttons/GhButton.vue'
 </script>
 <template>
   <div class="hero">
@@ -7,48 +8,76 @@
   </div>
 
   <div class="github-buttons">
-    <a href="https://pypi.org/project/Pincer">
-      <img src="https://img.shields.io/badge/dynamic/json?label=downloads&query=%24.total_downloads&url=https%3A%2F%2Fapi.pepy.tech%2Fapi%2Fprojects%2FPincer" alt="PyPI - Downloads">
-    </a>
-    <a href="https://pypi.org/project/Pincer">
-      <img src="https://img.shields.io/pypi/v/Pincer" alt="PyPI">
-    </a>
-    <a href="https://pypi.org/project/Pincer">
-      <img src="https://img.shields.io/pypi/pyversions/Pincer" alt="yPI - Python Version">
-    </a>
-    <a href="https://scrutinizer-ci.com/g/Pincer-org/pincer/?branch=main">
-      <img src="https://scrutinizer-ci.com/g/Pincer-org/pincer/badges/quality-score.png?b=main" alt="Scrutinizer Code Quality">
-    </a>
-    <a href="https://scrutinizer-ci.com/g/Pincer-org/Pincer/build-status/main">
-      <img src="https://scrutinizer-ci.com/g/Pincer-org/Pincer/badges/build.png?b=main" alt="Build Status">
-    </a>
-    <a href="https://pincer.readthedocs.io/en/latest/?badge=latest">
-      <img src="https://readthedocs.org/projects/pincer/badge/?version=latest" alt="Documentation Status">
-    </a>
-    <a href="https://codecov.io/gh/Pincer-org/Pincer">
-      <img src="https://codecov.io/gh/Pincer-org/Pincer/branch/main/graph/badge.svg?token=T15T34KOQW" alt="codecov">
-    </a>
-    <a href="/">
-      <img src="https://tokei.rs/b1/github/pincer-org/pincer?category=code&path=pincer" alt="Lines of code">
-    </a>
-    <a href="/">
-      <img src="https://img.shields.io/github/repo-size/Pincer-org/Pincer" alt="Repo Size">
-    </a>
-    <a href="/">
-      <img src="https://img.shields.io/github/last-commit/Pincer-org/Pincer" alt="GitHub last commit">
-    </a>
-    <a href="/">
-      <img src="https://img.shields.io/github/commit-activity/m/Pincer-org/Pincer?label=commits" alt="GitHub commit activity">
-    </a>
-    <a href="/">
-      <img src="https://img.shields.io/github/license/Pincer-org/Pincer" alt="GitHub">
-    </a>
-    <a href="https://discord.gg/pincer">
-      <img src="https://img.shields.io/discord/881531065859190804" alt="Discord">
-    </a>
-    <a href="https://github.com/psf/black">
-      <img src="https://img.shields.io/badge/code%20style-black-000000.svg" alt="Code style: black">
-    </a>
+    <GhButton
+        href="https://pypi.org/project/Pincer"
+        badge-url="https://img.shields.io/badge/dynamic/json?label=downloads&query=%24.total_downloads&url=https%3A%2F%2Fapi.pepy.tech%2Fapi%2Fprojects%2FPincer"
+        alt="PyPI - Downloads"
+    />
+    <GhButton
+        href="https://pypi.org/project/Pincer"
+        badge-url="https://img.shields.io/pypi/v/Pincer"
+        alt="PyPI"
+    />
+    <GhButton
+        href="https://pypi.org/project/Pincer"
+        badge-url="https://img.shields.io/pypi/pyversions/Pincer"
+        alt="PyPI - Python Version"
+    />
+    <GhButton
+        href="https://scrutinizer-ci.com/g/Pincer-org/pincer/?branch=main"
+        badge-url="https://scrutinizer-ci.com/g/Pincer-org/pincer/badges/quality-score.png?b=main"
+        alt="Scrutinizer Code Quality"
+    />
+    <GhButton
+        href="https://scrutinizer-ci.com/g/Pincer-org/Pincer/build-status/main"
+        badge-url="https://scrutinizer-ci.com/g/Pincer-org/Pincer/badges/build.png?b=main"
+        alt="Build Status"
+    />
+    <GhButton
+        href="https://pincer.readthedocs.io/en/latest/?badge=latest"
+        badge-url="https://readthedocs.org/projects/pincer/badge/?version=latest"
+        alt="Documentation Status"
+    />
+    <GhButton
+        href="https://codecov.io/gh/Pincer-org/Pincer"
+        badge-url="https://codecov.io/gh/Pincer-org/Pincer/branch/main/graph/badge.svg?token=T15T34KOQW"
+        alt="codecov"
+    />
+    <GhButton
+        href="/"
+        badge-url="https://tokei.rs/b1/github/pincer-org/pincer?category=code&path=pincer"
+        alt="Lines of code"
+    />
+    <GhButton
+        href="/"
+        badge-url="https://img.shields.io/github/repo-size/Pincer-org/Pincer"
+        alt="Repo Size"
+    />
+    <GhButton
+        href="/"
+        badge-url="https://img.shields.io/github/last-commit/Pincer-org/Pincer"
+        alt="GitHub last commit"
+    />
+    <GhButton
+        href="/"
+        badge-url="https://img.shields.io/github/commit-activity/m/Pincer-org/Pincer?label=commits"
+        alt="GitHub commit activity"
+    />
+    <GhButton
+      href="/"
+      badge-url="https://img.shields.io/github/license/Pincer-org/Pince"
+      alt="GitHub"
+    />
+    <GhButton
+      href="https://discord.gg/pince"
+      badge-url="https://img.shields.io/discord/881531065859190804"
+      alt="Discord"
+    />
+    <GhButton
+      href="https://github.com/psf/black"
+      badge-url="https://img.shields.io/badge/code%20style-black-000000.svg"
+      alt="Code style: black"
+    />
   </div>
 
   <p>
---
+++ src/components/buttons/GhButton.vue
@@ -0,0 +1,8 @@
+<script setup>
+defineProps({href: String, badgeUrl: String, alt: String})
+</script>
+<template>
+  <a v-bind:href=href>
+    <img v-bind:src=badgeUrl v-bind:alt=alt>
+  </a>
+</template>
