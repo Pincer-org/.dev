5acf6a2da9ab7be3f5d11833b7d6bab4675ba396
sigmanificient <sigmanificient@gmail.com>
2021-12-16 07:20:21 +0100

    :sparkles: Hamburger menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,7 +6,7 @@
         Pincer
       </a>
     </div>
-    <a id="hamburger" class="hamburger">
+    <a id="hamburger" class="hamburger" @click="menuOpen = !menuOpen" :class="{ 'rotated' : menuOpen }">
       <svg width="34" height="22" viewBox="0 0 34 22" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g stroke="black" stroke-linejoin="round" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round">
           <path d="M1.41516 1.79492H32.1134"/>
@@ -15,7 +15,7 @@
         </g>
       </svg>
     </a>
-    <nav>
+    <nav :class="{ 'open' : menuOpen }">
       <ul>
         <li>
           <a href="https://discord/pincer">discord</a>
@@ -53,8 +53,16 @@ a {
     gap: 20px;
   }
 
-  &.hamburger svg g {
-    stroke: var(--text-color)
+  &.hamburger {
+      svg g {
+        stroke: var(--text-color)
+      }
+
+    &.rotated {
+      transform: rotate(180deg);
+    }
+
+    transition: 0.3s ease-in-out;
   }
 
   font-weight: 500;
@@ -62,22 +70,43 @@ a {
 }
 
 nav {
+  top: 80px;
   background-color: var(--header-background-color-secondary);
   position: absolute;
-  top: 80px;
   width: 100%;
   left: 0;
-}
+  z-index: -1;
+  transform: translateY(-100%);
+  transition: transform 0.3s ease-in-out;
 
-nav ul {
-  margin: 40px 0;
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-  justify-content: center;
-}
+  &.open {
+    transform: translateY(0);
+  }
 
-nav li {
-  list-style: none;
+  ul {
+    margin: 40px 0;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    justify-content: center;
+  }
+
+  li {
+    list-style: none;
+  }
 }
 </style>
+<script>
+export default {
+  data() {
+    return {
+      menuOpen: false
+    }
+  },
+  methods: {
+    toggleMenu() {
+      this.menuOpen = !this.menuOpen
+    }
+  }
+}
+</script>
