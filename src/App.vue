d7a50de50504ef19a386c78ee8adde059ab1c039
sigmanificient <sigmanificient@gmail.com>
2021-12-16 07:02:27 +0100

    :building_construction: Page layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <script setup>
 import Header from "./components/Header.vue";
+import Hero from "./components/Hero.vue";
+import Pincer from "./components/Pincer.vue";
+import Team from "./components/Team.vue";
+import Related from "./components/Related.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <Header/>
+  <main>
+    <Hero/>
+    <Pincer/>
+    <Team/>
+    <Related/>
+  </main>
+  <Footer/>
 </template>
 
 <style lang="scss">
