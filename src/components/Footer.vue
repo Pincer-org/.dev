5378df992d162019667420c097f3c25bfd87d353
sigmanificient <sigmanificient@gmail.com>
2021-12-16 16:47:04 +0100

    :sparkles: Added basic footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,1 +1,21 @@
-<template></template>
+<template>
+    <footer class="footer">
+      <p>Copyright &copy; 2021 - Pincer-org</p>
+      <p>Site made by Sigmanificient</p>
+    </footer>
+</template>
+<style scoped>
+.footer {
+  display: flex;
+  background-color: var(--header-background-color);
+  justify-content: center;
+  align-items: center;
+  flex-flow: column wrap;
+  padding: 1rem;
+  gap: 1em;
+}
+
+p {
+  margin: 0;
+}
+</style>
