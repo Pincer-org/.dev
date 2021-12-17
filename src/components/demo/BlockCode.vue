926283360526be5c5663df275c88c14cd479946f
sigmanificient <sigmanificient@gmail.com>
2021-12-17 22:43:47 +0100

    :art: Get a more realistic token

--- src/components/demo/BlockCode.vue
+++ src/components/demo/BlockCode.vue
@@ -4,7 +4,7 @@ import T from './T.vue'
 <template>
   <figure>
     <figcaption>
-      <p></p>
+      <p>bot.py</p>
     </figcaption>
     <pre>
       <code>
@@ -16,33 +16,50 @@ import T from './T.vue'
 
   <T c="b">@command</T>(<T c="k">name</T><T c="p">=</T><T c="g">"say"</T>, <T c="k">description</T><T
   c="p">=</T><T c="g">"Send a custom message!"</T>)
-  <T c="p">async def</T> <T c="b">say_command</T>(<T c="r">self</T>, <T c="k">ctx</T><T c="p">:</T> MessageContext, <T
-  c="k">message</T><T c="k">:</T> <T c="b">str</T>)<T c="p">:</T>
+  <T c="p">async def</T> <T c="b">say_command</T>(<T c="r">self</T>, <T c="k">ctx</T><T c="p">:</T> MessageContext, <T c="k">message</T><T c="k">:</T> <T c="b">str</T>)<T c="p">:</T>
     <T c="p">return</T> <T c="b">Embed</T>(
-    <T c="k">description</T>=(
-      <T c="g">f"</T>{<T c="k">ctx</T>.author.mention}<T c="g"> said:\n"</T>
-      <T c="p">f"</T>`{<T c="k">message</T>}<T c="g">`"</T>
+      <T c="k">description</T>=(
+        <T c="g">f"</T>{<T c="k">ctx</T>.author.mention}<T c="g"> said:\n"</T>
+        <T c="p">f"</T>`{<T c="k">message</T>}<T c="g">`"</T>
+      )
     )
-  )
 
 
 <T c="p">if</T> __name__ <T c="p">==</T> <T c="g">"__main__"</T>:
-  bot = <T c="b">Bot</T>(<T c="k">token</T><T c="p">=</T><T c="g">"<span id="token">WfVc18N6kJAbMzMqGTE1NhT5.YVCuNA.5OrfKz5KADVhIlxMgXjSs4aQcMk</span>"</T>)
+  bot = <T c="b">Bot</T>(<T c="k">token</T><T c="p">=</T><T c="g">"<span id="token">super.secret.token</span>"</T>)
   bot.<T c="b">run</T>()
       </code>
     </pre>
   </figure>
 </template>
 <script>
+const base64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-'
+
+const getTokenPart = (length) => {
+  let token = ''
+  for (let i = 0; i < length; i++) {
+    token += base64[Math.floor(Math.random() * base64.length)]
+  }
+  return token
+}
+
+const getToken = () => {
+  return getTokenPart(24) + '.' + getTokenPart(6) + '.' + getTokenPart(27)
+}
+
+
 window.onload = () => {
   const token = document.getElementById("token");
+  const x = window.matchMedia("(min-width: 720px)")
+
+  if (!x.matches) return
+
+  // Take 24 random char from base64
+  const tokenValue = Array.from({ length: 24 }, () => base64[Math.floor(Math.random() * base64.length)]).join('')
+
 
   setInterval(() => {
-        token.innerHTML = (
-            Math.random().toString(24).substring(2)
-            + Math.random().toString(6).substring(1)
-            + Math.random().toString(27).substring(1)
-        );
+        token.innerHTML = getToken();
       }, 3000
   );
 }
