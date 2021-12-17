7bd66342d2388ccf85bd99c2f5f709bcbd711712
sigmanificient <sigmanificient@gmail.com>
2021-12-17 21:19:39 +0100

    :dizzy: Added token animation

--- src/components/demo/BlockCode.vue
+++ src/components/demo/BlockCode.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import T from './T.vue'
+import T from './T.vue'
 </script>
 <template>
   <figure>
@@ -14,8 +14,10 @@
 
         <T c="p">class</T> <T c="y">Bot</T>(Client)<T c="p">:</T>
 
-          <T c="b">@command</T>(<T c="k">name</T><T c="p">=</T><T c="g">"say"</T>, <T c="k">description</T><T c="p">=</T><T c="g">"Send a custom message!"</T>)
-          <T c="p">async def</T> <T c="b">say_command</T>(<T c="r">self</T>, <T c="k">ctx</T><T c="p">:</T> MessageContext, <T c="k">message</T><T c="k">:</T> <T c="b">str</T>)<T c="p">:</T>
+          <T c="b">@command</T>(<T c="k">name</T><T c="p">=</T><T c="g">"say"</T>, <T c="k">description</T><T
+          c="p">=</T><T c="g">"Send a custom message!"</T>)
+          <T c="p">async def</T> <T c="b">say_command</T>(<T c="r">self</T>, <T c="k">ctx</T><T c="p">:</T> MessageContext, <T
+          c="k">message</T><T c="k">:</T> <T c="b">str</T>)<T c="p">:</T>
             <T c="p">return</T> <T c="b">Embed</T>(
             <T c="k">description</T>=(
               <T c="g">f"</T>{<T c="k">ctx</T>.author.mention}<T c="g"> said:\n"</T>
@@ -25,12 +27,26 @@
 
 
         <T c="p">if</T> __name__ <T c="p">==</T> <T c="g">"__main__"</T>:
-          bot = <T c="b">Bot</T>(<T c="k">token</T><T c="p">=</T><T c="g">"WfVc18N6kJAbMzMqGTE1NhT5.YVCuNA.5OrfKz5KADVhIlxMgXjSs4aQcMk"</T>)
+          bot = <T c="b">Bot</T>(<T c="k">token</T><T c="p">=</T><T c="g">"<span id="token">WfVc18N6kJAbMzMqGTE1NhT5.YVCuNA.5OrfKz5KADVhIlxMgXjSs4aQcMk</span>"</T>)
           bot.<T c="b">run</T>()
       </code>
     </pre>
   </figure>
 </template>
+<script>
+window.onload = () => {
+  const token = document.getElementById("token");
+
+  setInterval(() => {
+        token.innerHTML = (
+            Math.random().toString(24).substring(2)
+            + Math.random().toString(6).substring(1)
+            + Math.random().toString(27).substring(1)
+        );
+      }, 3000
+  );
+}
+</script>
 <style scoped>
 code {
   white-space: pre;
