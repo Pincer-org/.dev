c7224867b2b8d8d56b240b460486ee0a49c33335
sigmanificient <sigmanificient@gmail.com>
2021-12-31 22:23:48 +0100

    :recycle: Vue 2.6

--- src/components/Related.vue
+++ src/components/Related.vue
@@ -1,26 +1,25 @@
-<script setup>
-import BlueButton from '../components/buttons/BlueButton.vue'
-</script>
 <template>
-  <h2>Related projects</h2>
+  <div class="related">
+    <h2>Related projects</h2>
 
-  <div class="left">
-    <h3>The Pincer CLI</h3>
-    <p>
-      In the intent to simplify pincer user life, we are building a console line interface tools that will bring
-      automation to the pincer package. Commands to import templates, create cogs and commands are going to make the
-      developer experience easier and straight forward, faster and open to beginners.
-    </p>
-    <BlueButton text="Github Repository" link="https://github.com/Pincer-org/cli"/>
-  </div>
-  <div class="right">
-    <h3>The Pincer Bot</h3>
-    <p>
-      Our discord server need a powerful yet fun bot to interact with and demonstrate the key features of our library.
-      It will also be a good example for an advanced Bot, running in production, that new users can explore to
-      understand how they can use the library and take advantage of it.
-    </p>
-    <BlueButton text="Github Repository" link="https://github.com/Pincer-org/bot"/>
+    <div class="left">
+      <h3>The Pincer CLI</h3>
+      <p>
+        In the intent to simplify pincer user life, we are building a console line interface tools that will bring
+        automation to the pincer package. Commands to import templates, create cogs and commands are going to make the
+        developer experience easier and straight forward, faster and open to beginners.
+      </p>
+      <BlueButton text="Github Repository" link="https://github.com/Pincer-org/cli"/>
+    </div>
+    <div class="right">
+      <h3>The Pincer Bot</h3>
+      <p>
+        Our discord server need a powerful yet fun bot to interact with and demonstrate the key features of our library.
+        It will also be a good example for an advanced Bot, running in production, that new users can explore to
+        understand how they can use the library and take advantage of it.
+      </p>
+      <BlueButton text="Github Repository" link="https://github.com/Pincer-org/bot"/>
+    </div>
   </div>
 </template>
 <style lang="scss" scoped>
@@ -33,7 +32,12 @@ p {
 }
 </style>
 <script>
+import BlueButton from '../components/buttons/BlueButton.vue';
+
 export default {
-  name: 'Related'
+  name: 'Related',
+  components: {
+    BlueButton,
+  }
 }
 </script>
