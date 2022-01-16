88405f91598589136f0e13e3239f9f1b7e88ce21
sigmanificient <edhyjox@gmail.com>
2022-01-17 00:37:58 +0100

    :recycle: improving Command

--- src/components/demo/Command.vue
+++ src/components/demo/Command.vue
@@ -1,13 +1,13 @@
 <template>
 	<div class="command">
-		<div class="up">
+		<div class="command-up">
 			<img src="@/assets/logo/icon.svg" alt="Pincer logo">
 			<header>
 				<h1>/say <span class="block">message</span></h1>
 				<p>Send a custom message!</p>
 			</header>
 		</div>
-		<div class="down">
+		<div class="command-down">
 			<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 				<circle cx="12" cy="12" r="12" fill="#C4C4C4"/>
 				<rect x="5" y="10.5" width="14" height="3" fill="#40444B"/>
@@ -27,50 +27,50 @@ export default {
 <style lang="scss" scoped>
 .command {
 	font-size: .5em;
-}
 
-.up, .down {
-	display: flex;
-	align-items: center;
-	justify-content: flex-start;
-	padding: 0.75rem 0.75rem 0.75rem 1rem;
-	gap: 0.75rem;
-
-	p {
-		font-size: 1.25em;
-		font-weight: 500;
-		margin: 0;
-	}
-}
-
-.up {
-	background-color: var(--bg-color-secondary);
-	border-top-left-radius: .5em;
-	border-top-right-radius: .5em;
-
-	h1 {
-		font-size: 1.25em;
-		font-weight: 500;
+	&-up, &-down {
 		display: flex;
 		align-items: center;
-		margin: 0 0 4px;
-		gap: 4px
+		justify-content: flex-start;
+		padding: 0.75rem 0.75rem 0.75rem 1rem;
+		gap: 0.75rem;
+
+		p {
+			font-size: 1.25em;
+			font-weight: 500;
+			margin: 0;
+		}
 	}
 
-	img {
-		height: 1.5rem;
+	&-up {
+		background-color: var(--bg-color-secondary);
+		border-top-left-radius: .5em;
+		border-top-right-radius: .5em;
+
+		h1 {
+			font-size: 1.25em;
+			font-weight: 500;
+			display: flex;
+			align-items: center;
+			margin: 0 0 4px;
+			gap: 4px
+		}
+
+		img {
+			height: 1.5rem;
+		}
 	}
-}
 
-.down {
-	background-color: var(--discord-color-lighter);
-	border-bottom-left-radius: .5em;
-	border-bottom-right-radius: .5em;
-	padding-left: 3em;
-	gap: 1rem;
+	&-down {
+		background-color: var(--discord-color-lighter);
+		border-bottom-left-radius: .5em;
+		border-bottom-right-radius: .5em;
+		padding-left: 3em;
+		gap: 1rem;
 
-	svg {
-		width: 2em;
+		svg {
+			width: 2em;
+		}
 	}
 }
 
